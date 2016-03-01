75d5789ed8d730153a3602b0d2825ee8c6a1cd6d
Andrew Furth <andrew.furth@gmail.com>
2016-02-29 18:24:46 -0800

    completed random colors every two sseconds

--- script.js
+++ script.js
@@ -45,11 +45,39 @@
 //***************************************************************************
 //GRADIENT
 //***************************************************************************
+// var body = document.querySelector('body');
+
+// body.style.margin = '0';
+
+
+
+// for (i = 0; i < 63; i++) {
+//   var square = document.createElement('div');
+//   body.appendChild(square);
+//   square.style.width = '11.1%';
+//   square.style.float='left';
+//   square.style.display='block';
+//   square.style.paddingBottom = '11.1%';
+//   var randomColor1 = Math.floor(Math.random()*255);
+//   var randomColor2 = Math.floor(Math.random()*255);
+//   var randomColor3 = Math.floor(Math.random()*255);
+//   var opacity = (100-i)/100;
+//   if (i % 2 === 0) {
+//     square.style.backgroundColor = `rgba(${255},${255}, ${0}, ${opacity})`;
+//   } else {
+//     square.style.backgroundColor = `rgba(${0},${255}, ${255}, ${opacity})`;
+//   }
+// };
+
+//***************************************************************************
+//RANDOM COLORS W/ TIMER
+//***************************************************************************
+
 var body = document.querySelector('body');
 
 body.style.margin = '0';
 
-
+var colorArray = [];
 
 for (i = 0; i < 63; i++) {
   var square = document.createElement('div');
@@ -61,10 +89,16 @@ for (i = 0; i < 63; i++) {
   var randomColor1 = Math.floor(Math.random()*255);
   var randomColor2 = Math.floor(Math.random()*255);
   var randomColor3 = Math.floor(Math.random()*255);
-  var opacity = (100-i)/100;
-  if (i % 2 === 0) {
-    square.style.backgroundColor = `rgba(${255},${255}, ${0}, ${opacity})`;
-  } else {
-    square.style.backgroundColor = `rgba(${0},${255}, ${255}, ${opacity})`;
-  }
+  square.style.backgroundColor = `rgb(${randomColor1},${randomColor2}, ${randomColor3})`;
+  colorArray.push(square);
 };
+
+setInterval(function() {
+  for (i = 0; i < 63; i++) {
+    var randomColor1 = Math.floor(Math.random()*255);
+    var randomColor2 = Math.floor(Math.random()*255);
+    var randomColor3 = Math.floor(Math.random()*255);  
+    colorArray[i].style.backgroundColor = `rgb(${randomColor1},${randomColor2}, ${randomColor3})`;
+  }
+}, 2000);
+
