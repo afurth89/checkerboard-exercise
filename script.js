6f1a54055eec4b9b701d48b331895b317ea7190a
Andrew Furth <andrew.furth@gmail.com>
2016-02-29 18:05:15 -0800

    random colors and gradient also added

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
+//***************************************************************************
+//CHECKERBOARD
+//***************************************************************************
+
+// var body = document.querySelector('body');
+
+// body.style.margin = '0';
+
+// for (i = 0; i < 63; i++) {
+//   var square = document.createElement('div');
+//   body.appendChild(square);
+//   square.style.width = '11.1%';
+//   square.style.float='left';
+//   square.style.display='block';
+//   square.style.paddingBottom = '11.1%';
+//   if (i % 2 === 0) {
+//     square.style.backgroundColor = 'black';
+//   } else {
+//     square.style.backgroundColor = 'red';
+//   }
+// }
+
+//***************************************************************************
+//RANDOM COLORS
+//***************************************************************************
+// var body = document.querySelector('body');
+
+// body.style.margin = '0';
+
+
+
+// for (i = 0; i < 63; i++) {
+//   var square = document.createElement('div');
+//   body.appendChild(square);
+//   square.style.width = '11.1%';
+//   square.style.float='left';
+//   square.style.display='block';
+//   square.style.paddingBottom = '11.1%';
+//   var randomColor1 = Math.floor(Math.random()*255);
+//   var randomColor2 = Math.floor(Math.random()*255);
+//   var randomColor3 = Math.floor(Math.random()*255);
+//   square.style.backgroundColor = `rgb(${randomColor1},${randomColor2}, ${randomColor3})`;
+// };
+
+//***************************************************************************
+//GRADIENT
+//***************************************************************************
 var body = document.querySelector('body');
 
 body.style.margin = '0';
+
+
 
 for (i = 0; i < 63; i++) {
   var square = document.createElement('div');
@@ -9,9 +58,13 @@ for (i = 0; i < 63; i++) {
   square.style.float='left';
   square.style.display='block';
   square.style.paddingBottom = '11.1%';
+  var randomColor1 = Math.floor(Math.random()*255);
+  var randomColor2 = Math.floor(Math.random()*255);
+  var randomColor3 = Math.floor(Math.random()*255);
+  var opacity = (100-i)/100;
   if (i % 2 === 0) {
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = `rgba(${255},${255}, ${0}, ${opacity})`;
   } else {
-    square.style.backgroundColor = 'red';
+    square.style.backgroundColor = `rgba(${0},${255}, ${255}, ${opacity})`;
   }
-}
+};
