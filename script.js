5cb65693738e495ce7c4b9249c7f27c0f3511be5
Andrew Furth <andrew.furth@gmail.com>
2016-02-29 17:04:28 -0800

    checkerboard created

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+var body = document.querySelector('body');
+
+body.style.margin = '0';
+
+for (i = 0; i < 63; i++) {
+  var square = document.createElement('div');
+  body.appendChild(square);
+  square.style.width = '11.1%';
+  square.style.float='left';
+  square.style.display='block';
+  square.style.paddingBottom = '11.1%';
+  if (i % 2 === 0) {
+    square.style.backgroundColor = 'black';
+  } else {
+    square.style.backgroundColor = 'red';
+  }
+}
